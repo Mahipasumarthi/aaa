9bcc807d8ce7723820f2ab0b8aecc37a1b7c8de4
Mahipasumarthi <163092140+Mahipasumarthi@users.noreply.github.com>
2024-03-20 23:43:27 +0530

    indentation changed

--- src/main/java/Programmer16Addition.java
+++ src/main/java/Programmer16Addition.java
@@ -3,7 +3,7 @@ package com.qualizeal.javaapp.mathematics;
  public class Programmer16Addition {
 	public int add() {
 	    int num1 = 1;
-		int num2 = 4;
+	    int num2 = 4;
 		
 		int result = num1 + num2;
 		return result;
